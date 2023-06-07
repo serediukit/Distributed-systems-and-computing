87c3c09d8b01bf62a0749bae480bf31649119d5e
Valentyn Serediuk <serediukit@gmail.com>
2023-06-07 18:08:22 +0300

    done lab3

---
+++ Lab3/ComSrv/DBComSrv.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Lab3.ComSrv
+{
+
+    [Guid("F53DD0D3-448C-43CD-B3C3-C115E6EFBAC5")]
+    public interface IDataLayer
+    {
+        DataTable GetTable(string connectionString, string table);
+        List<string> GetTables(string connectionString);
+        void InsertData(string connectionString, string tableName, string parameters, string data);
+        void DeleteDataById(string connectionString, string tableName, string idColumnName, int id);
+        DataTable FindDataByValue(string connectionString, string tableName, string columnName, string columnValue);
+        DataTable FindDataById(string connectionString, string tableName, string idColumnName, int idValue);
+        void EditDataById(string connectionString, string tableName, string idColumnName, int idValue, string parameters, string data);
+        DataTable FindData(string connectionString, string tableName, string filterExpression);
+        DataTable employee_list(string connectionString);
+        DataTable order_list(string connectionString);
+    }
+
+
+
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class DBComSrv : IDataLayer
+    {
+        static void Main(string[] args)
+        {
+            DBComSrv dataBase = new DBComSrv();
+            string conectionName = @"Provider=SQLOLEDB;Data Source=PC-XIAOMIPRO15;Integrated Security=SSPI;Initial Catalog=FOOD_DELIVERE_MAIN_OFFICE";
+            dataBase.order_list(conectionName);
+
+
+
+
+
+        }
+        public DBComSrv() { }
+
+        public void InsertData(string connectionString, string tableName, string parameters, string data)
+        {
+
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                string cmd = $"INSERT INTO {tableName} ({parameters}) VALUES ('{data}')";
+                Console.WriteLine(cmd);
+                OleDbCommand command = new OleDbCommand(cmd, connection);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteDataById(string connectionString, string tableName, string idColumnName, int id)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                OleDbCommand command = new OleDbCommand($"DELETE FROM {tableName} WHERE {idColumnName} ={id}", connection);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public List<string> GetTables(string connectionString)
+        {
+            List<string> ls = new List<string>();
+            ls.Clear();
+            OleDbConnection cn = new OleDbConnection(connectionString);
+            cn.Open();
+            OleDbCommand cmd = new OleDbCommand("SELECT * FROM sys.tables", cn);
+            OleDbDataReader r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                ls.Add(r.GetString(0));
+            }
+            cn.Close();
+            return ls;
+        }
+
+        public DataTable GetTable(string connectionString, string table)
+        {
+            OleDbConnection cn = new OleDbConnection(connectionString);
+            cn.Open();
+            OleDbCommand cmd = new OleDbCommand("SELECT * FROM " + table, cn);
+            OleDbDataReader r = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(r);
+            cn.Close();
+            return dt;
+        }
+
+        public DataTable FindDataByValue(string connectionString, string tableName, string columnName, string columnValue)
+        {
+
+            OleDbConnection connection = new OleDbConnection(connectionString);
+            connection.Open();
+            OleDbCommand command = new OleDbCommand($"SELECT * FROM {tableName} WHERE {columnName} ={columnValue}", connection);
+            DataTable dt = new DataTable();
+            OleDbDataReader r = command.ExecuteReader();
+            dt.Load(r);
+            connection.Close();
+            return dt;
+
+        }
+
+        public DataTable FindDataById(string connectionString, string tableName, string idColumnName, int idValue)
+        {
+            OleDbConnection connection = new OleDbConnection(connectionString);
+            connection.Open();
+            OleDbCommand command = new OleDbCommand($"SELECT * FROM {tableName} WHERE {idColumnName} = {idValue}", connection);
+            DataTable dt = new DataTable();
+            OleDbDataReader r = command.ExecuteReader();
+            dt.Load(r);
+            connection.Close();
+            return dt;
+        }
+
+        public void EditDataById(string connectionString, string tableName, string idColumnName, int idValue, string parameters, string data)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                OleDbCommand command = new OleDbCommand($"UPDATE {tableName} SET {parameters} = {data}  WHERE {idColumnName} = {idValue}", connection);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public DataTable FindData(string connectionString, string tableName, string filterExpression)
+        {
+            OleDbConnection connection = new OleDbConnection(connectionString);
+
+            connection.Open();
+            OleDbCommand command = new OleDbCommand($"SELECT * FROM {tableName} WHERE {filterExpression}", connection);
+            DataTable dt = new DataTable();
+            OleDbDataReader r = command.ExecuteReader();
+            dt.Load(r);
+            connection.Close();
+            return dt;
+
+        }
+
+        public DataTable employee_list(string connectionString)
+        {
+            OleDbConnection connection = new(connectionString);
+
+            connection.Open();
+            string commandText = $"SELECT \r\n    e.firstname, e.middlename, e.lastname, e.address,e.phone_number, \r\n    d.name, \r\n    p.name \r\nFROM \r\n    FOOD_DELIVERY_MAIN_OFFICE.dbo.employee AS e\r\n    INNER JOIN FOOD_DELIVERY_MAIN_OFFICE.dbo.department AS d ON e.id_department = d.id\r\n    INNER JOIN FOOD_DELIVERY_MAIN_OFFICE.dbo.position AS p ON e.id_position = p.id \r\n\tUNION\r\n\tSELECT \r\n    e.firstname, e.middlename, e.lastname, e.address, e.phone_number, \r\n    d.name, \r\n    p.name \r\nFROM \r\n    [PC-XIAOMIPRO15\\MSSQLSERVER2].FOOD_DELIVERY_BRANCHE.dbo.employee AS e\r\n    INNER JOIN [PC-XIAOMIPRO15\\MSSQLSERVER2].FOOD_DELIVERY_BRANCHE.dbo.department AS d ON e.id_department = d.id\r\n    INNER JOIN [PC-XIAOMIPRO15\\MSSQLSERVER2].FOOD_DELIVERY_BRANCHE.dbo.position AS p ON e.id_position = p.id;\r\n";
+            OleDbCommand command = new OleDbCommand(commandText, connection);
+            DataTable dt = new DataTable();
+            OleDbDataReader r = command.ExecuteReader();
+            dt.Load(r);
+            connection.Close();
+            return dt;
+
+        }
+
+        public DataTable order_list(string connectionString)
+        {
+            try
+            {
+                OleDbConnection connection = new OleDbConnection(connectionString);
+                connection.Open();
+                string commandText = $"\tSELECT _order.id, _order.order_list, _order.destination_adress, act_of_order._date, act_of_order.payment_amoun, act_of_order.is_done\r\nFROM FOOD_DELIVERY_MAIN_OFFICE.dbo._order\r\nJOIN FOOD_DELIVERY_MAIN_OFFICE.dbo.act_of_order ON _order.id = act_of_order.id_order\r\n\tUNION\r\n\tSELECT _order.id, _order.order_list, _order.destination_adress, act_of_order._date, act_of_order.payment_amoun, act_of_order.is_done\r\nFROM [PC-XIAOMIPRO15\\MSSQLSERVER2].FOOD_DELIVERY_BRANCHE.dbo._order\r\nJOIN [PC-XIAOMIPRO15\\MSSQLSERVER2].FOOD_DELIVERY_BRANCHE.dbo.act_of_order ON _order.id = act_of_order.id_order;";
+                OleDbCommand command = new OleDbCommand(commandText, connection);
+                DataTable dt = new DataTable();
+                OleDbDataReader r = command.ExecuteReader();
+                dt.Load(r);
+                connection.Close();
+                return dt;
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return null;
+        }
+
+    }
+
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,183 @@
+﻿using Lab3.ComSrv;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Reflection;
+
+
+namespace Lab3.client
+{
+    [ComImport, Guid("F53DD0D3-448C-43CD-B3C3-C115E6EFBAC5")]
+
+
+
+    public interface IDataLayer
+    {
+        DataTable GetTable(string ConStr, string Table);
+        List<string> GetTables(string ConStr);
+        void InsertData(string connectionString, string tableName, string parameters, string data);
+        void DeleteDataById(string connectionString, string tableName, string idColumnName, int id);
+        DataTable FindDataByValue(string connectionString, string tableName, string columnName, string columnValue);
+        DataTable FindDataById(string connectionString, string tableName, string idColumnName, int idValue);
+        void EditDataById(string connectionString, string tableName, string idColumnName, int idValue, string parameters, string data);
+        DataTable FindData(string connectionString, string tableName, string filterExpression);
+        DataTable employee_list(string connectionString);
+        DataTable order_list(string connectionString);
+
+    }
+
+    public partial class Form1 : Form
+    {
+        private IDataLayer comObject = new DBComSrv() as IDataLayer;
+        Type type;
+        Object ComSrv;
+
+        MethodInfo mi;
+        string connection = @"Provider=SQLOLEDB;Data Source=PC-XIAOMIPRO15;Integrated Security=SSPI;Initial Catalog=FOOD_DELIVARY_MAIN_OFFICE";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            type = Type.GetTypeFromProgID("Lab3.ComSrv.DBComSrv");
+            if (type != null)
+            {
+                ComSrv = Activator.CreateInstance(type);
+                if (ComSrv != null)
+                {
+                    MessageBox.Show("ComSrv is created");
+                }
+                else
+                    MessageBox.Show("ComSrv is not created");
+            }
+            else
+                MessageBox.Show("ComSrv type is not found");
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            mi = type.GetMethod("GetTables");
+            List<string> ls = (List<string>)mi.Invoke(ComSrv, new object[] { connection });
+            DataTable dt = new DataTable();
+            dt.Columns.Add("tables");
+
+
+            foreach (string table in ls)
+            {
+                dt.Rows.Add(table);
+            }
+
+
+            resultintable.DataSource = dt;
+
+            Console.Write(ls);
+
+        }
+        public void get_table_by_name(object sender, EventArgs e)
+        {
+            string tname = table_name.Text;
+            mi = type.GetMethod("GetTable");
+            DataTable dt = (DataTable)mi.Invoke(ComSrv, new object[] { connection, tname });
+            resultintable.DataSource = dt;
+
+        }
+        private void insert_data_Click(object sender, EventArgs e)
+        {
+            string tn = table_name.Text;
+            string ps = parameters.Text;
+            string ds = data.Text;
+            mi = type.GetMethod("InsertData");
+            DataTable dt = (DataTable)mi.Invoke(ComSrv, new object[] { connection, tn, ps, ds });
+            resultintable.DataSource = dt;
+        }
+
+        private void find_by_id_Click(object sender, EventArgs e)
+        {
+            string tn = table_name.Text;
+            string idc = "id";
+            string id_string = table_id.Text;
+            int id = int.Parse(id_string);
+            mi = type.GetMethod("FindDataById");
+            DataTable dt = (DataTable)mi.Invoke(ComSrv, new object[] { connection, tn, idc, id });
+            resultintable.DataSource = dt;
+        }
+
+        private void delete_by_id_Click(object sender, EventArgs e)
+        {
+            string tn = table_name.Text;
+            string idc = "id";
+            string id_string = table_id.Text;
+            int id = int.Parse(id_string);
+            mi = type.GetMethod("DeleteDataById");
+            DataTable dt = (DataTable)mi.Invoke(ComSrv, new object[] { connection, tn, idc, id });
+            resultintable.DataSource = dt;
+
+        }
+
+        private void find_by_value_Click(object sender, EventArgs e)
+        {
+            string tn = table_name.Text;
+            string cn = column_name.Text;
+            string cv = table_value.Text;
+            mi = type.GetMethod("FindDataByValue");
+            DataTable dt = (DataTable)mi.Invoke(ComSrv, new object[] { connection, tn, cn, cv });
+            resultintable.DataSource = dt;
+
+        }
+
+        private void edit_Click(object sender, EventArgs e)
+        {
+            string tn = table_name.Text;
+            string idc = "id";
+            string id_string = table_id.Text;
+            int id = int.Parse(id_string);
+            string ps = parameters.Text;
+            string ds = data.Text;
+            mi = type.GetMethod("EditDataById");
+            DataTable dt = (DataTable)mi.Invoke(ComSrv, new object[] { connection, tn, idc, id, ps, ds });
+            resultintable.DataSource = dt;
+
+        }
+
+        private void find_by_filter_Click(object sender, EventArgs e)
+        {
+            string tn = table_name.Text;
+            string ft = filter.Text;
+            mi = type.GetMethod("FindData");
+            DataTable dt = (DataTable)mi.Invoke(ComSrv, new object[] { connection, tn, ft });
+            resultintable.DataSource = dt;
+
+
+        }
+        private void employee_list_Click(object sender, EventArgs e)
+        {
+            mi = type.GetMethod("employee_list");
+            DataTable dt = (DataTable)mi.Invoke(ComSrv, new object[] { connection });
+            resultintable.DataSource = dt;
+
+        }
+
+
+        private void order_list_Click(object sender, EventArgs e)
+        {
+            mi = type.GetMethod("order_list");
+            DataTable dt = (DataTable)mi.Invoke(ComSrv, new object[] { connection });
+            resultintable.DataSource = dt;
+        }
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+}
